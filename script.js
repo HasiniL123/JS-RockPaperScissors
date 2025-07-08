251d9bdcb33a6ddfc3525abb46ffa425fcf9435b
HasiniL123 <130543697+HasiniL123@users.noreply.github.com>
2025-07-08 15:11:10 +0800

    Add functions to get Human and Computer choice

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log('HelloWorld')
+ 
+
+let Hscore, Cscore = 0;
+function getComputerChoice(){
+
+    let x;
+    let Cchoice;
+
+    x = Math.random()
+    
+
+    if (x <= (1/3)){
+        Cchoice = "rock";
+    }else if (x> (1/3) && x <= (2/3)) {
+    Cchoice = "paper";
+    }else{
+        Cchoice = "scissors"
+    }
+
+    
+
+    return Cchoice;
+}
+
+
+
+
+ function getHumanChoice(){
+
+    let Hchoice;
+
+    Hchoice = prompt("What is your choice?")
+    
+    return Hchoice;
+}
+
+
+
+
+
+
+let Hchoice = getHumanChoice().toLowerCase();
+console.log("Hchoice:" + Hchoice);
+
+let Cchoice = getComputerChoice();
+console.log("Cchoice:" + Cchoice);
+
+console.log(playRound(Hchoice, Cchoice)) 
+
